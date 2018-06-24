6db85488e27144eee0eb52658c2fc5068f5d861e
timotree3 <timorcb@gmail.com>
2018-06-24 17:04:10 -0400

    wip

---
+++ first-app/src/hello_world.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div><h1>"Hello,"</h1>
+    <h1 style={"margin-left": "40px"}>"World"
+    </h1></div>
+    
+    , document.getElementById('root'));
---
+++ first-app/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+class Calendar extends React.Component {
+    render() {
+        return(
+            <div class="calendar">
+            <h1>January</h1>
+                <table>
+                    <Weeklydays/>
+                </table>
+            </div>
+        );
+    }
+}
+class Weeklydays extends React.Component {
+    render() {
+        const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+        return(
+            <tr>{for (day of days) {}}</tr>
+        );
+    }
+}
+ReactDOM.render(
+    <Calendar/>
+    , document.getElementById('root'));
