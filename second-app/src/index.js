f556c48cb063ff3b36141b3b785a9f155c25ac57
timotree3 <timorcb@gmail.com>
2018-06-26 13:35:00 -0400

    wip

--- second-app/src/index.js
+++ second-app/src/index.js
@@ -10,27 +10,61 @@ class Calendar extends React.Component {
 
     render() {
         return(
-            <div class="calendar">
-            <h1>January</h1>
-                <table>
-                    {makeRow(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])}
-                    {/* <MonthDays date={this.state.date}/> */}
-                    {/* {"{1+1}"} */}
+            <div className="calendar">
+            <h1>{this.state.date.toLocaleDateString('en-US', {month: 'long'})}</h1>
+                <table border="1">
+                    <thead>
+                        {makeRow(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"], "ASDASD")}
+                    </thead>
+                    <MonthDays date={this.state.date}/>
                 </table>
             </div>
         );
     }
 }
 
-// class MonthDays extends React.Component {
-//     render() {
+class MonthDays extends React.Component {
+    render() {
+        let date = this.props.date;
+        let month = date.getMonth();
+        date.setDate(1);
+        // JS week starts sunday. Reset start of week to monday.
+        let weekday = ((date.getDay()-1) % 7 + 7) % 7;
+        date.setTime(date.getTime()-weekday*(1000*60*60*24));
+        // reset date to the most recent monday
+        let rows = [];
+        for (let i = 0; true; i++) {
+            console.log(date);
+            rows.push([]);
+            for (let j = 0; j < 7; j++) {
+                rows[i].push(date.getDate());
+                date.setTime(date.getTime()+(1000*60*60*24));
+            }
+            if (date.getMonth() !== month) {
+                console.log(date);
+                console.log(month);
+                console.log(`breaking at ${rows.length} rows`);
+                break;
+            }
+        }
+        return (
+            <tbody>
+                {rows.map((row, i) => makeRow(row, i))}
+            </tbody>
+        );
+    }
+}
 
-//     }
-// }
-
-const makeRow = function(items) {
+const makeRow = function(items, rowKey) {
+    console.log(rowKey);
     return (
-        <tr><td>{items.join("</td><td>")}</td></tr>
+        <tr key={rowKey}>
+            {
+                items.map(
+                    (item, i)=><td key={i}>{item}</td>
+                )
+            }
+        </tr>
     );
 };
 
